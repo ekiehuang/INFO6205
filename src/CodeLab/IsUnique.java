c70d23a0acfb5ce03b6f734d006b684a3477830e
Huang Ekie <ekie@Huangs-MacBook-Pro.local>
2020-09-14 12:06:46 -0700

    finish isUnique

--- src/CodeLab/IsUnique.java
+++ src/CodeLab/IsUnique.java
@@ -1,2 +1,15 @@
+import java.util.HashSet;
+
 public class IsUnique {
+    public static void main(String[] args){
+        System.out.println(new IsUnique().isUnique("abcd"));
+    }
+    public boolean isUnique(String s){
+        HashSet<Character> set=new HashSet<>();
+        for(int i=0;i<s.length();i++){
+            if(set.contains(s.charAt(i))) return false;
+            set.add(s.charAt(i));
+        }
+        return true;
+    }
 }

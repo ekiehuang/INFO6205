c6a8ff7221e8f490f76395b70358b18517463c46
ekiehuang <ekie729@gmail.com>
2020-09-14 11:04:28 -0700

    temporary

--- src/CodeLab/CheckPermutation.java
+++ src/CodeLab/CheckPermutation.java
@@ -1,2 +1,13 @@
+import java.util.HashSet;
+
 public class CheckPermutation {
+  HashSet<String> set=new HashSet<>();
+  public boolean checkPermutation(String s1, String s2){
+    int from=0,to=s1.length()-1;
+    permutation(s1,from,to);
+    return set.contains(s2);
 }
+
+  private void permutation(String s1, int from, int to) {
+  }
+  }

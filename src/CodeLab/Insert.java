f557cb05eb2a8cf47d67eeaf85359984509fe010
ekiehuang <ekie729@gmail.com>
2020-09-17 22:49:16 -0700

    finish insert

--- src/CodeLab/Insert.java
+++ src/CodeLab/Insert.java
@@ -1,2 +1,24 @@
 public class Insert {
+  public Node insert(Node head, int insertVal) {
+    Node r = new Node(insertVal,null);
+
+    if(head == null){
+      r.next = r;
+      return r;
+    }
+
+    Node n = head;
+    while(true){
+      if((insertVal >= n.val && insertVal <= n.next.val)
+              || (n.next.val < n.val && (insertVal >= n.val || insertVal <= n.next.val))
+              || n.next == head){
+        r.next = n.next;
+        n.next = r;
+        break;
+      }
+      n = n.next;
+    }
+
+    return head;
+  }
 }

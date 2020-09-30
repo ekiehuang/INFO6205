7449e30d3ebad27302b0b825aff320ae11f87782
ekiehuang <ekie729@gmail.com>
2020-09-30 15:43:53 -0700

    lc385..

---
+++ src/CodeLab/Week2/NestedInteger.java
@@ -0,0 +1,16 @@
+/*
+import java.util.List;
+
+public interface NestedInteger {
+
+     public NestedInteger();
+
+
+    public NestedInteger(int value);
+    public boolean isInteger();
+public Integer getInteger();
+  public void setInteger(int value);
+public void add(NestedInteger ni);
+public List<NestedInteger> getList();
+}
+*/

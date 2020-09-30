77525d4ac58b541a452f53ed9f50ea9f8412e367
ekiehuang <ekie729@gmail.com>
2020-09-30 15:19:19 -0700

    lc 155 updated

--- src/CodeLab/Week2/MinStack.java
+++ src/CodeLab/Week2/MinStack.java
@@ -4,13 +4,17 @@ import java.util.LinkedList;
 import java.util.Stack;
 
 public class MinStack{
-  ArrayList<Integer> arr=new ArrayList<>();
+  //Approach 1: Two stacks
+  private Stack<Integer> stack=new Stack<>();
   private Stack<Integer> minStack=new Stack<>();
+  //Approach 2: less efficient
+ /* private ArrayList<Integer> arr=new ArrayList<>();
+  private Stack<Integer> minStack=new Stack<>();*/
   public static void main(String[] args){
     MinStack ms=new MinStack();
-    ms.push(-2);
     ms.push(0);
-    ms.push(-3);
+    ms.push(1);
+    ms.push(0);
     System.out.println(ms.getMin());
     //ms.push(1);
     ms.pop();
@@ -22,28 +26,47 @@ public class MinStack{
   }
 
   public void push(int x) {
-    minStack.push(x);
-    arr.add(x);
+    //Approach 1:
+    stack.push(x);
+    if(minStack.isEmpty()||x<=minStack.peek()) minStack.push(x);
+
+    //Approach 2
+    /*minStack.push(x);
+    arr.add(x);*/
   }
 
   public void pop() {
-    if(minStack.isEmpty()) return;
+    //Approach 1:
+    if(stack.isEmpty()) return;
+    int p=stack.pop();
+    if(p==minStack.peek()) minStack.pop();
+
+    //Approach 2:
+    /*if(minStack.isEmpty()) return;
     arr.remove(minStack.size()-1);
-    minStack.pop();
+    minStack.pop();*/
 
 
   }
 
   public int top() {
-    return minStack.peek();
+    //Approach 1:
+    return stack.peek();
+
+    //Approach 2:
+    //return minStack.peek();
   }
 
   public int getMin() {
-    if(arr.size()==0) return Integer.MIN_VALUE;
+    //Approach 1:
+    return minStack.peek();
+
+    //Approach 2:
+    /*if(arr.size()==0) return Integer.MIN_VALUE;
     int min=arr.get(0);
     for(Integer i:arr){
       if(i<min) min=i;
     }
-    return min;
+    return min;*/
   }
 }
